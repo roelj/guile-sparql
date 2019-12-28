99b6502060b763de542ca85e1389d07a97747016
Giacomo Leidi <goodoldpaul@autistici.org>
2019-12-28 19:06:23 +0100

    lang: Export construct and ask.
    * sparql/lang.scm [exports]: Add construct and ask.

--- sparql/lang.scm
+++ sparql/lang.scm
@@ -21,7 +21,7 @@
   #:use-module (ice-9 format)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-26)
-  #:export (prefix select insert-data delete-data create))
+  #:export (prefix select construct ask insert-data delete-data create))
 
 ;;
 ;; UTILITIES
