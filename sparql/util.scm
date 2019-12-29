769eaa62c14e8b4f77abb1414c83b7e83c346284
Roel Janssen <roel@gnu.org>
2019-12-29 21:40:01 +0100

    util: Add ‘query-results->alist’ and ‘csv-read-entry’.
    * sparql/util.scm (query-results->alist): New variable;
      (csv-read-entry): New variable.

--- sparql/util.scm
+++ sparql/util.scm
@@ -1,4 +1,4 @@
-;;; Copyright © 2018 Roel Janssen <roel@gnu.org>
+;;; Copyright © 2018, 2019 Roel Janssen <roel@gnu.org>
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify it
 ;;; under the terms of the GNU General Public License as published by
@@ -17,11 +17,19 @@
   #:use-module (ice-9 receive)
   #:use-module (ice-9 rdelim)
   #:use-module (ice-9 format)
+  #:use-module (srfi srfi-1)
   #:use-module (web response)
+
+  ;; The following functions are used in the macros for ‘query-results->list’
+  ;; and ‘query-results->alist’.  We re-export them from this module so that
+  ;; these modules need not to be imported when using these macros.
+  #:re-export (response-code receive read-line)
+
   #:export (display-query-results
             display-query-results-of
             query-results->list
-            split-line
+            query-results->alist
+            csv-read-entry
             uri-suffix
             uri-base))
 
@@ -50,44 +58,88 @@
                 (response-code header)
                 (read-line port)))))
 
-(define* (query-results-to-list port #:optional (output '()))
+(define* (query-results-to-list port #:optional (skip-first-line? #f)
+                                                (output '()))
   "Returns a list of data read from PORT."
-  (let* ((line   (read-line port)))
-    (if (eof-object? line)
+  (when skip-first-line? (csv-read-entry port))
+  (let* [(entry (csv-read-entry port))]
+    (if (null? entry)
         (reverse output)
-        (query-results-to-list port
-         (cons (map (lambda (item) (string-trim-both item #\"))
-                    (string-split line #\,))
-               output)))))
+        (query-results-to-list port #f (cons entry output)))))
 
 (define-syntax-rule
-  (query-results->list query)
+  (query-results->list query skip-first-line?)
   (receive (header port)
       query
     (if (= (response-code header) 200)
-        (query-results-to-list port)
-        (format #t "Error (~a): ~a~%"
-                (response-code header)
-                (read-line port)))))
+        (query-results-to-list port skip-first-line?)
+        (begin
+          (format #t "Error (~a): ~a~%"
+                  (response-code header)
+                  (read-line port))
+          #f))))
 
-(define* (split-line line #:optional (delimiter #\,))
-  "Splits LINE where DELIMITER is the separator, properly handling quotes."
+(define* (query-results-to-alist port #:optional (header '())
+                                      (output '()))
+  (let [(entry (csv-read-entry port))]
+    (if (null? entry)
+        (reverse output)
+        (if (null? header)
+            (query-results-to-alist port entry output)
+            (query-results-to-alist port header
+              (cons
+               (map (lambda (t) `(,(list-ref t 0) . ,(list-ref t 1)))
+                    (zip header entry))
+               output))))))
 
-  (define (iterator line length token-begin position in-quote tokens)
+(define-syntax-rule
+  (query-results->alist query)
+  (receive (header port)
+      query
+    (if (= (response-code header) 200)
+        (query-results-to-alist port)
+        (begin
+          (format #t "Error (~a): ~a~%"
+                  (response-code header)
+                  (read-line port))
+          #f))))
+
+(define* (csv-read-entry port
+                         #:optional
+                         (delimiter #\,)
+                         (current-token '())
+                         (tokens '())
+                         (in-quote #f))
+  (let [(character (read-char port))]
     (cond
-     ((= position length)
-      (reverse (cons (string-drop line token-begin) tokens)))
-     ((eq? (string-ref line position) delimiter)
-      (if in-quote
-          (iterator line length token-begin (1+ position) in-quote tokens)
-          (iterator line length (1+ position) (1+ position) in-quote
-                    (cons (substring line token-begin position)
-                          tokens))))
-     ((eq? (string-ref line position) #\")
-      (iterator line length token-begin (1+ position) (not in-quote) tokens))
-     (else (iterator line length token-begin (1+ position) in-quote tokens))))
-
-  (iterator line (string-length line) 0 0 #f '()))
+     [(or (eof-object? character)
+          (and (eq? character #\newline)
+               (not in-quote)))
+      (if (and (null? current-token)
+               (null? tokens))
+          current-token
+          (map (lambda (item)
+                 (let [(item-length (string-length item))]
+                   (cond
+                    [(and (> item-length 1)
+                          (eq? (string-ref item 0) #\")
+                          (eq? (string-ref item (- item-length 1)) #\"))
+                     (substring item 1 (- item-length 1))]
+                    [else item])))
+               (reverse (cons (list->string (reverse current-token)) tokens))))]
+     [(eq? character #\")
+      (if (and (not (null? current-token))
+               (or (eq? (car current-token) #\\)
+                   (eq? (car current-token) #\")))
+          (csv-read-entry port delimiter (cons character (cdr current-token)) tokens in-quote)
+          (csv-read-entry port delimiter (cons character current-token) tokens (not in-quote)))]
+     [(and (eq? character delimiter)
+           (not in-quote))
+      (csv-read-entry port delimiter '()
+                      (cons (list->string (reverse current-token)) tokens)
+                      in-quote)]
+     [else
+      (csv-read-entry port delimiter (cons character current-token) tokens in-quote)])))
 
 (define (uri-suffix input)
   (catch #t
