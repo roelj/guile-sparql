2a13ad5283630f9917094319d1ca9383bbb6a6dc
Giacomo Leidi <goodoldpaul@autistici.org>
2020-05-20 22:47:51 +0200

    tests: Refactor to use with-test-group.
    * tests/test-lang.scm: Use with-test-group macro to create a session
      for each one of the tested function.

--- tests/test-lang.scm
+++ tests/test-lang.scm
@@ -20,89 +20,101 @@
 
 (test-runner-factory sparql:test-runner)
 
-(test-begin "test-lang")
-
 ;; Triples visitor
 
 (define triples->pattern
   (@@ (sparql lang) triples->pattern))
 
-(test-equal "\n{\n}\n"
-  (triples->pattern '()))
+(with-test-group "triples->pattern-tests"
+ (lambda _
+   (test-equal "\n{\n}\n"
+     (triples->pattern '()))
 
-(test-equal
-    "\n{\n  ?s rdf:type ?o .\n}\n"
-  (triples->pattern
-   '((s "rdf:type" o))))
+   (test-equal
+       "\n{\n  ?s rdf:type ?o .\n}\n"
+     (triples->pattern
+      '((s "rdf:type" o))))
 
-(test-equal
-    "\n{\nOPTIONAL \n{\n  ?s rdf:type ?o .\n}\n}\n"
-  (triples->pattern
-   '((optional
-      ((s "rdf:type" o))))))
+   (test-equal
+       "\n{\nOPTIONAL \n{\n  ?s rdf:type ?o .\n}\n}\n"
+     (triples->pattern
+      '((optional
+         ((s "rdf:type" o))))))
 
-(test-equal
-    "\n{\nMINUS \n{\n  ?s rdf:type ?o .\n  ?o rdf:type ?s .\n}\n}\n"
-  (triples->pattern
-   '((minus
-      ((s "rdf:type" o)
-       (o "rdf:type" s))))))
+   (test-equal
+       "\n{\nMINUS \n{\n  ?s rdf:type ?o .\n  ?o rdf:type ?s .\n}\n}\n"
+     (triples->pattern
+      '((minus
+         ((s "rdf:type" o)
+          (o "rdf:type" s))))))))
 
 ;; Prefix
 
-(test-equal "<http://purl.org/dc/elements/1.1/subject>"
-  ((prefix "http://purl.org/dc/elements/1.1/") "subject"))
+(with-test-group "prefix-test"
+ (lambda _
+   (test-equal "<http://purl.org/dc/elements/1.1/subject>"
+     ((prefix "http://purl.org/dc/elements/1.1/") "subject"))))
 
 ;; Where
 
 (define where (@@ (sparql lang) where))
 
-(test-equal "WHERE \n{\n}\n"
-  (where '()))
+(with-test-group "where-tests"
+ (lambda _
+   (test-equal "WHERE \n{\n}\n"
+     (where '()))
 
-(test-equal "FROM NAMED <http://www.example.org#graph> WHERE \n{\n}\n"
-  (where '()
-         #:graph "http://www.example.org#graph"
-         #:named #t))
+   (test-equal "FROM NAMED <http://www.example.org#graph> WHERE \n{\n}\n"
+     (where '()
+            #:graph "http://www.example.org#graph"
+            #:named #t))
 
-(test-equal "WHERE \n{\n}\nLIMIT 20 \nGROUP BY ?country \n"
-  (where '()
-         '((limit "20")
-           (group by country))))
+   (test-equal "WHERE \n{\n}\nLIMIT 20 \nGROUP BY ?country \n"
+     (where '()
+            '((limit "20")
+              (group by country))))))
 
 ;; Select
 
-(test-equal "SELECT DISTINCT \nWHERE \n{\n}\n"
-  (select '() '() #:distinct #t))
+(with-test-group "select-tests"
+ (lambda _
+   (test-equal "SELECT DISTINCT \nWHERE \n{\n}\n"
+     (select '() '() #:distinct #t))))
 
 ;; Construct
 
-(test-equal "CONSTRUCT WHERE \n{\n}\n"
-  (construct '() '()))
+(with-test-group "construct-tests"
+ (lambda _
+   (test-equal "CONSTRUCT WHERE \n{\n}\n"
+     (construct '() '()))
 
-(test-equal "CONSTRUCT \n{\n  ?s rdf:type ?o .\n}\nWHERE \n{\n}\n"
-  (construct '((s "rdf:type" o)) '()))
+   (test-equal "CONSTRUCT \n{\n  ?s rdf:type ?o .\n}\nWHERE \n{\n}\n"
+     (construct '((s "rdf:type" o)) '()))))
 
 ;; Ask
 
-(test-equal "ASK \n{\n}\n"
-  (ask '()))
+(with-test-group "ask-tests"
+ (lambda _
+   (test-equal "ASK \n{\n}\n"
+     (ask '()))
 
-(test-equal "ASK FROM <http://www.example.org#graph> \n{\n}\n"
-  (ask '()
-       #:graph "http://www.example.org#graph"))
+   (test-equal "ASK FROM <http://www.example.org#graph> \n{\n}\n"
+     (ask '()
+          #:graph "http://www.example.org#graph"))))
 
 ;; Modify Data
 
 (define modify-data (@@ (sparql lang) modify-data))
 
-(test-equal "DELETE DATA {\n  GRAPH <> {\n  }\n}\n"
-  (modify-data '() 'DELETE #:graph ""))
+(with-test-group "modify-data-tests"
+ (lambda _
+   (test-equal "DELETE DATA {\n  GRAPH <> {\n  }\n}\n"
+     (modify-data '() 'DELETE #:graph ""))
 
-(test-equal "INSERT DATA {\n  GRAPH <http://www.example.org#graph> {\n    ?s rdf:type ?o .\n  }\n}\n"
-  (modify-data '((s "rdf:type" o)) 'INSERT
-               #:graph "http://www.example.org#graph"))
+   (test-equal "INSERT DATA {\n  GRAPH <http://www.example.org#graph> {\n    ?s rdf:type ?o .\n  }\n}\n"
+     (modify-data '((s "rdf:type" o)) 'INSERT
+                  #:graph "http://www.example.org#graph"))
 
-(test-error #t (modify-data 'DELETE '((s "rdf:type" o))))
+   (test-error #t (modify-data 'DELETE '((s "rdf:type" o))))))
 
-(exit (if (test-end "test-lang") 0 1))
+(exit (zero? (test-runner-fail-count (test-runner-current))))
